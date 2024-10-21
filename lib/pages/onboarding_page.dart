2ad9c07b115c8cc9f4211f52fa2bbd027ea13142
RahulShaw321 <R.xhawss321@gmail.com>
2024-10-21 08:27:28 +0530

    Initial Commit

---
+++ lib/pages/onboarding_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(body: Stack(children: [
+
+    //Horizontal Scrollable Pages
+    PageView(children: [],)
+
+    //Notes
+
+    //Dot Navigation SmoothPageIndicator
+    //Join the Movement Button
+    //Login Button
+
+
+
+    ],) );
+  }
+}
